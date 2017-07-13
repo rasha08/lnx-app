f54b83fd3368c2c29b06c4e44511555d015980c9
rasha08 <rasha08@gmail.com>
2017-07-13 19:08:41 +0200

    fixed memory leak on Messenger.js

--- src/Messenger.js
+++ src/Messenger.js
@@ -1,80 +1,99 @@
-import React,  { Component } from 'react';
-import { database, auth, googleAuthProvider } from './firebase'
-import map from 'lodash/map'
-import SingleMessage from './SingleMessage'
-import NewMessage from './NewMessage'
+import React, { Component } from 'react';
+import { database, auth, googleAuthProvider } from './firebase';
+import map from 'lodash/map';
+import SingleMessage from './SingleMessage';
+import NewMessage from './NewMessage';
 
 class Messenger extends Component {
-	constructor(props){
-		super()
+  constructor(props) {
+    super();
 
-		this.state = {
-			currentUser: auth.currentUser,
-			messages: null
-		}
-		this.messagesRef = database.ref('/messages')
-		this.sendMessage = this.sendMessage.bind(this)
-	}
+    this.state = {
+      currentUser: auth.currentUser,
+      messages: null
+    };
+    this.messagesRef = database.ref('/messages');
+    this.sendMessage = this.sendMessage.bind(this);
+    this.signin = this.signin.bind(this);
+    this.signout = this.signout.bind(this);
+  }
 
-	componentDidMount(){
-		auth.onAuthStateChanged((currentUser)=>{
-	  	this.setState({currentUser})
+  componentDidMount() {
+    auth.onAuthStateChanged(currentUser => {
+      this.setState({ currentUser });
 
-		this.messagesRef.orderByChild("text").on('value', (snapshot)=>{
-			this.setState({
-				messages: snapshot.val()
-			})
-		})
-				})
-	}
-	signin(){
-		auth.signInWithPopup(googleAuthProvider)
-			this.setState({
-				currentUser:auth.currentUser
-			})
-	}
-	signout(){
-		auth.signOut()
-	}
-	sendMessage(message){
-			
-			if(message){
-			this.messagesRef.push({
-			text:message,
-			user:this.state.currentUser.displayName,
-			date:new Date()
-		})
-			}
-		else{
-			return
-		}
-		
-		
-	}
-
-	render(){
-		if(this.state.messages && this.state.currentUser){
-			return(
-				<div className="container col s12">
-					{map(this.state.messages, message=><SingleMessage message={message} key={message.date}/>)}
-					<NewMessage sendMessage={this.sendMessage}/>
-					<button className="btn waves-effect waves-light right logout" onClick={()=>{this.signout()}}>LOGOUT
-							<i className="material-icons right">person</i>
-						</button>
-				</div>
-			)
-		}
-		return  (
-		<div className="container">
-		  <div className="progress black">
-            <div className="indeterminate red"/>
-          </div>
-			<button className="btn waves-effect waves-light" onClick={()=>{this.signin()}}>LOGIN
-							<i className="material-icons right">person</i>
-						</button>
-		</div> 
-	)
-	}
+      this.messagesRef.orderByChild('text').on('value', snapshot => {
+        this.setState({
+          messages: snapshot.val()
+        });
+      });
+    });
+  }
+  signin() {
+    auth.signInWithPopup(googleAuthProvider).then(
+      this.setState({
+        currentUser: auth.currentUser
+      })
+    );
+  }
+  signout() {
+    auth.signOut();
+  }
+  sendMessage(message) {
+    if (message) {
+      this.messagesRef.push({
+        text: message,
+        user: this.state.currentUser.displayName,
+        date: new Date()
+      });
+    } else {
+      return;
+    }
+  }
+  componentWillUnmount() {
+    window.removeEventListener('click', this.signin);
+    window.removeEventListener('click', this.signout);
+  }
+  render() {
+    if (this.state.messages && this.state.currentUser) {
+      return (
+        <div className="container col s12">
+          {map(this.state.messages, message => <SingleMessage message={message} key={message.date} />)}
+          <NewMessage sendMessage={this.sendMessage} />
+          <button
+            className="btn waves-effect waves-light right logout"
+            onClick={() => {
+              this.signout();
+            }}
+          >
+            LOGOUT
+            <i className="material-icons right">person</i>
+          </button>
+        </div>
+      );
+    } else if (!this.state.messages && this.state.currentUser) {
+      return (
+        <div className="container col s12">
+          <NewMessage sendMessage={this.sendMessage} />
+          <button className="btn waves-effect waves-light right logout" onClick={this.signout}>
+            LOGOUT
+            <i className="material-icons right">person</i>
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div className="container">
+        <div className="progress black">
+          <div className="indeterminate red" />
+        </div>
+        <button className="btn waves-effect waves-light" onClick={this.signin}>
+          LOGIN
+          <i className="material-icons right">person</i>
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Messenger
+export default Messenger;
--- src/NewMessage.js
+++ src/NewMessage.js
@@ -1,37 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class NewMessage extends Component{
-	constructor(props){
-		super();
+class NewMessage extends Component {
+  constructor(props) {
+    super();
 
-		this.state = {
-			message: '',
-			props
-		}
-		this.sendMessage = this.sendMessage.bind(this)
-	}
+    this.state = {
+      message: '',
+      props
+    };
+    this.sendMessage = this.sendMessage.bind(this);
+  }
 
-	sendMessage(message){
-		this.state.props.sendMessage(this.state.message)
-	}
-	render(){
-		return (
-			<div className="container">
-				<div className="row new-message">
-					<div className="input-field col s9">
-          				<textarea placeholder="Mesto za poruku" onChange={(event)=>{
-							  this.setState({message: event.target.value})
-						  }} className="materialize-textarea"></textarea>
-        			</div>
-					<div className="col s3">
-						<button className="btn waves-effect waves-light" onClick={()=>{this.sendMessage(this.state.message)}}>Submit
-							<i className="material-icons right">send</i>
-						</button>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  sendMessage() {
+    this.state.props.sendMessage(this.state.message);
+    this.setState({
+      message: ''
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.sendMessage);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row new-message">
+          <div className="input-field col s9">
+            <textarea
+              placeholder="Mesto za poruku"
+              onChange={event => {
+                this.setState({ message: event.target.value });
+              }}
+              className="materialize-textarea"
+            />
+          </div>
+          <div className="col s3">
+            <button className="btn waves-effect waves-light" onClick={this.sendMessage}>
+              Submit
+              <i className="material-icons right">send</i>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewMessage
+export default NewMessage;
